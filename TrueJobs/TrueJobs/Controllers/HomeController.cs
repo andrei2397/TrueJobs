bfa5d881279dc12dc4604cd8fe67b0c5431caa9f
andrey2397 <andrei.barbu97@gmail.com>
2018-08-04 19:41:24 +0300

    Home Index information

--- TrueJobs/TrueJobs/Controllers/HomeController.cs
+++ TrueJobs/TrueJobs/Controllers/HomeController.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
 
 namespace TrueJobs.Controllers
 {
     public class HomeController : Controller
     {
+
+        private JobsEntities2 db = new JobsEntities2();
         public ActionResult Index()
         {
+
+            var userCount = db.Users.Count();
+            var companyCount = db.Companies.Count();
+            var jobsCount = db.Jobs.Count();
+            var interestsCount = db.Interests.Count();
+
+            ViewBag.userCount = userCount;
+            ViewBag.companyCount = companyCount;
+            ViewBag.jobsCount = jobsCount;
+            ViewBag.interestsCount = interestsCount;
             return View();
         }
 
